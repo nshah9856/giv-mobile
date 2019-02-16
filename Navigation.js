3b2557fe87716f058344797f26fddede510d9846
Haven King <havenmking@gmail.com>
2019-02-15 23:24:42 -0800

    Changed navbar framework and added haptic feedback.

--- Navigation.js
+++ Navigation.js
@@ -1,15 +1,44 @@
-import NavigationBar from 'react-native-navbar'
 import React from 'react'
-import { StyleSheet, Text, View, Button, StatusBar} from 'react-native';
+import { StyleSheet, Text, View, Button, StatusBar, Vibration } from 'react-native';
+import { Header , Icon} from 'react-native-elements'
+
+const Settings = props => (
+    <View>
+        <Icon 
+            name='menu'
+            underlayColor='transparent'
+            color='#fff'
+            onPress= {
+                () => {
+                    Vibration.vibrate(10)
+                }
+            }
+        />
+    </View>
+)
+
+const Profile = props => (
+    <View>
+        <Icon 
+            name='person'
+            underlayColor='transparent'
+            color='#fff'
+            onPress= {
+                () => {
+                    Vibration.vibrate(10)
+                }
+            }
+        />
+    </View>
+)
 
 const NavBar = props => (
     <View>
         <StatusBar hidden/>
-        <NavigationBar
-            title={<Text style={styles.title} >Sharity</Text>}
-            rightButton={<Button title="Profile" onPress={()=>console.log("Profile pressed!")}></Button>}
-            leftButton={<Button title="Settings" onPress={()=>console.log("Settings Pressed!")}></Button>}
-
+        <Header
+        leftComponent={<Settings/>}
+        centerComponent={{ text: 'Sharity', style: { color: '#fff' } }}
+        rightComponent={<Profile/>}
         />
     </View>
 )
