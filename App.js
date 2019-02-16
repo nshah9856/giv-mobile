f5ca20697cd9836754867da8d2a359d685934b3d
Nisarg Shah <npshah9856@gmail.com>
2019-02-16 13:16:56 -0800

    current master

--- App.js
+++ App.js
@@ -1,59 +1,15 @@
-import React from 'react';
-import { StyleSheet, Button, StatusBar, View } from 'react-native';
 import {createDrawerNavigator, createAppContainer} from 'react-navigation';
-import { Icon } from 'react-native-elements'
 
 import HomeScreen from './components/HomeScreen';
 import SettingsScreen from './components/SettingsScreen';
 import ProfileScreen from './components/ProfileScreen';
 import PaymentScreen from './components/PaymentScreen';
-
-class MyHomeScreen extends React.Component {
-  static navigationOptions = {
-    drawerLabel: 'Home',
-    drawerIcon: ({ tintColor }) => (
-      <Icon
-        name='menu'
-        color='#ff0000'
-      />
-    ),
-  };
-
-  render() {
-    return (
-      <Button
-        onPress={() => this.props.navigation.navigate('Notifications')}
-        title="Go to notifications"
-      />
-    );
-  }
-}
-
-class MyProfileScreen extends React.Component {
-  static navigationOptions = {
-    drawerLabel: 'Notifications',
-    drawerIcon: ({ tintColor }) => (
-      <Icon
-        name='menu'
-        color='#ff0000'
-      />
-    ),
-  };
-
-  render() {
-    return (
-      <View>
-        <StatusBar hidden/>
-        <Button
-          onPress={() => this.props.navigation.openDrawer()}
-          title="Go back home"
-        />
-      </View>
-    );
-  }
-}
+import LoginScreen from './components/LoginScreen';
 
 const MyDrawerNavigator = createDrawerNavigator({
+  Login: {
+    screen: LoginScreen,
+  },
   Home: {
     screen: HomeScreen,
   },
