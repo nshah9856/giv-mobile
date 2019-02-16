4acfa5ef232a1927ae4a0c0b918e4707c7e6e82b
Nisarg Shah <npshah9856@gmail.com>
2019-02-16 08:24:37 -0800

    Database is linked up now

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -5,10 +5,14 @@ import Swiper from 'react-native-swiper'
 import {Video} from 'expo'
 
 const { width, height } = Dimensions.get('window')
+import { Query } from "react-apollo";
+import ApolloClient, { gql } from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
 
 const CardComponent = props => (
     <Card containerStyle={{padding:0, margin: 0}} height="100%">
-        <ScrollView height="90%%">
+        <ScrollView height="90%">
             <Video
                 source={{ uri: props.VideoURL }}
                 shouldPlay
@@ -21,21 +25,60 @@ const CardComponent = props => (
     </Card>
 )
 
-const DisplayData = props => (
-    <Swiper           
-        dot={<View style={{backgroundColor: 'rgba(0,0,0,.2)', width: 5, height: 5, borderRadius: 4, marginLeft: 3, marginRight: 3, marginTop: 3, marginBottom: 3}} />}
-        activeDot={<View style={{backgroundColor: '#000', width: 8, height: 8, borderRadius: 4, marginLeft: 3, marginRight: 3, marginTop: 3, marginBottom: 3}} />}
-    >
-        <CardComponent 
-            VideoURL='http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4' 
-            description='There should be an image here!'
-        />
-        <CardComponent 
-            VideoURL='http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4' 
-            description='There should be an image here!'
-        />
-    </Swiper>
-)
+
+
+class DisplayData extends React.Component {
+
+    constructor(){
+        super()
+        this.state = {data: null, loading: true}
+    }
+    componentDidMount() {
+        this.setState({loading:true})
+        this.fetchData()
+        .then(data => 
+            data.json())
+        .then(data => {
+            this.setState({data:data.data.organizations, loading:false})
+        }
+        )
+    }
+
+    fetchData(){
+        return fetch('https://hackuci19-231913.appspot.com/graphql?query={organizations{title}}')
+    }
+    render(){
+        if (this.state.loading === true){
+            return(
+                <View>
+                    <Text>
+                        Loading....
+                    </Text>
+                </View>
+            )
+        }
+        else{
+        return(
+        <Swiper           
+            dot={<View style={{backgroundColor: 'rgba(0,0,0,.2)', width: 5, height: 5, borderRadius: 4, marginLeft: 3, marginRight: 3, marginTop: 3, marginBottom: 3}} />}
+            activeDot={<View style={{backgroundColor: '#000', width: 8, height: 8, borderRadius: 4, marginLeft: 3, marginRight: 3, marginTop: 3, marginBottom: 3}} />}
+        >
+            {
+                this.state.data.map(({title}) => {
+                    console.log(title)
+                return <CardComponent 
+                    VideoURL='http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4' 
+                    description={<Text>{title}</Text>}
+                    key = {1}
+                 />
+                }
+                )
+            }
+        </Swiper>
+        )
+        }
+    }
+}
 
 class HomeScreen extends React.Component {
     static navigationOptions = function(props) {
@@ -57,10 +100,10 @@ class HomeScreen extends React.Component {
 
     render() {
         return (
-            <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-                <StatusBar hidden/>
-                <DisplayData />
-            </View>
+                <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+                    <StatusBar hidden/>
+                    <DisplayData />
+                </View>
         );
     }
 }
